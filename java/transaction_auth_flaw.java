e9763eec348e87b32408e731b91656efc2372608
risknav <137483023+risknav@users.noreply.github.com>
2025-06-29 11:22:24 +0530

    Delete java directory

--- java/transaction_auth_flaw.java
+++
@@ -1,7 +0,0 @@
-// VULNERABLE CODE
-public void sellGold(String userId, double amount) {
-    // No check if current user can access this userId's account
-    GoldAccount account = database.getAccount(userId);
-    account.sellGold(amount);
-    transferFunds(amount, targetAccount);
-}
